c2fad902943ae069b36bf27e06131699bc365cdc
Nguyen Van Hiep <hiep12092001@gmail.com>
2020-08-13 11:57:04 +0700

    update detail✨

--- index.js
+++ index.js
@@ -49,7 +49,9 @@ const apiTracker = (data) => {
                         }
                     ]
                 },
-                detail
+                detail: {
+                    data: detail
+                }
             }
         })
     })
